985691f0f897b89a574124590a10186eb43bf712
Jair Marques <jairsmarques1126@gmail.com>
2024-11-12 12:00:00 -0300

    adicionando a aula sobre tipos de variáveis

---
+++ estrutura_sequencial/src/TiposBasicosVariaveis.java
@@ -0,0 +1,20 @@
+
+public class TiposBasicosVariaveis {
+
+	public static void main(String[] args) {
+		
+		String nome = "Maria";
+		int idade = 45;
+		double altura = 1.70;
+		char sexo = 'F';
+		boolean verdadeiro = true;
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(sexo);
+		System.out.println(verdadeiro);
+
+	}
+
+}
