f902568be36c60a047bfe4df36b76fb422db8277
Jair Marques <jairsmarques1126@gmail.com>
2024-11-20 10:24:35 -0300

    finalizando o programa e adicionando os metodos

--- programa_conta_bancaria/src/entities/Account.java
+++ programa_conta_bancaria/src/entities/Account.java
@@ -33,12 +33,20 @@ public class Account {
 		return balance;
 	}
 	
-	
 	public void deposit(double amount) {
 		balance += amount;
 	}
 	
 	public void withdraw(double amount) {
 		balance -= amount + 5.0;
+	}
+	
+	public String toString() {
+		return "Account "
+				+ number
+				+ ", Holder: "
+				+ holder
+				+ ", Balance: $ "
+				+ String.format("%.2f", balance);
 	}
 }
