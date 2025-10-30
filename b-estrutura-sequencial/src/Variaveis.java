d3b31842c792da853dda13eb8f3bca70b141e99b
Jair Marques <jairsmarques1126@gmail.com>
2025-10-30 16:05:20 -0300

    adicionando os exemplos de código da aula sobre estrutura sequencial

---
+++ b-estrutura-sequencial/src/Variaveis.java
@@ -0,0 +1,18 @@
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		int idade = 25;
+		double altura = 1.68;
+		char sexo = 'F';
+		String nome = "Maria"; // cadeia de caracteres(palavras ou textos)
+
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(sexo);
+		System.out.println(nome);
+
+	}
+
+}
