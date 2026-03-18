b802ff17d60f080967dddbd6e2fb081544a1aee8
Jair Marques <jairsmarques1126@gmail.com>
2026-03-18 10:12:18 -0300

    adicionando encapsulamento ao código do programa

--- 7.0_construtores_palavra_this_sobrecarga_encapsulamento/src/entities/Product.java
+++ 7.0_construtores_palavra_this_sobrecarga_encapsulamento/src/entities/Product.java
@@ -2,14 +2,14 @@ package entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
-	
+	private String name;
+	private double price;
+	private int quantity;
+
 	public Product() {
-		
+
 	}
-	
+
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
@@ -19,6 +19,26 @@ public class Product {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
 	}
 
 	public double totalValueInStock() {
@@ -34,7 +54,12 @@ public class Product {
 	}
 
 	public String toString() {
-		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+		return name 
+				+ ", $ " 
+				+ String.format("%.2f", price) 
+				+ ", " 
+				+ quantity 
+				+ " units, Total: $ "
 				+ String.format("%.2f", totalValueInStock());
 	}
 }
