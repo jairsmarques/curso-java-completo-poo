b403247c558f2f8333f883e4494585af1d0f03ac
Jair de Sousa <jairsmarques1126@gmail.com>
2026-09-08 13:47:29 -0300

    adicionando os códigos da aula de estrutura sequencial

---
+++ secao02-estrutura-sequencial/src/ProcessamentoDeDados.java
@@ -0,0 +1,18 @@
+
+public class ProcessamentoDeDados {
+
+	public static void main(String[] args) {
+
+		double b, B, h, area;
+
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+
+		area = (b + B) / 2.0 * h; // processmento de dados
+
+		System.out.println(area);
+
+	}
+
+}
---
+++ secao02-estrutura-sequencial/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+
+public class TiposVariaveis {
+
+	public static void main(String[] args) {
+
+		// 08 tipos primitivos
+		// byte, short, int , long, char, float, double e boolean
+		// Uma variável possui nome(ou identificador), tipo e valor(opcional)
+		// String -> cadeia de caracteres
+
+		int idade = 25;
+		double altura = 1.68;
+		char sexo = 'F';
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(sexo);
+
+	}
+
+}
